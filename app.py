4b5f7f6c56eb00b48cdc3c8ca8d33475f0ab6669
Brian McKenna <D00197352@student.dkit.ie>
2021-11-06 17:53:44 +0000

    LOGIN / REG FORMS - Started working on form validation using WTForms, also removed login and reg forms from macros to their own file - BRIAN

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import InputRequired, data_required, Email, Length, DataRequired, EqualTo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SecretKey'
+formData = {}
+
+
+# Forms
+class LoginForm(FlaskForm):
+    inputEmail = StringField('Email Address', validators=[InputRequired(), Length(min=8, max=80), Email()])
+    inputPassword = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80, message="minimum 8 chars")])
+    submit = SubmitField('Submit')
+
+
+class RegisterForm(FlaskForm):
+    inputFullName = StringField('Full Name', validators=[InputRequired(), Length(min=8, max=80)])
+    inputEmail = StringField('Email Address', validators=[InputRequired(), Length(min=8, max=80), Email()])
+    inputPassword = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80, message="minimum 8 chars"), EqualTo('inputPasswordRepeat', message='Passwords must match')])
+    inputPasswordRepeat = PasswordField('Repeat Password')
+    submit = SubmitField('Submit')
 
 # ROUTES NOT CORRECT, JUST DOING BELOW TO VIEW WEBPAGES QUICKLY
 
@@ -12,12 +32,22 @@ def index():
 
 @app.route('/register')
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.inputEmail.data + ' ' + form.inputPassword.data + '</h1>'
+    else:
+        return render_template("register.html", form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.inputEmail.data + ' ' + form.inputPassword.data + '</h1>'
+    else:
+        return render_template("login.html", form=form)
 
 
 @app.route('/user')
