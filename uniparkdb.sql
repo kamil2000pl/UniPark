91d0f5c31f44be2953bfb7e0d2b0c97ef012f334
jacqueline <D00233669@student.dkit.ie>
2021-11-25 15:26:29 +0000

    fixed database grammar, renamed user table to driver because user is a reserved word in mysql

--- uniparkdb.sql
+++ uniparkdb.sql
@@ -1,17 +1,17 @@
 DROP database IF EXISTS uniparkdb;
 CREATE database IF NOT EXISTS uniparkdb;
 USE uniparkdb;
-DROP TABLE IF EXISTS car_entry, car_exit, transaction_history, payments, cars, users, locations, accounts;
+DROP TABLE IF EXISTS car_entry, car_exit, transaction_history, payment, car, driver, location, account;
 
-CREATE TABLE accounts (
+CREATE TABLE account (
 	account_id INT AUTO_INCREMENT NOT NULL,
 	account_balance DOUBLE(4, 2),
 	email_address VARCHAR(30),
 	password VARCHAR(120),
     PRIMARY KEY (account_id));
-    ALTER TABLE accounts AUTO_INCREMENT=1000;
+    ALTER TABLE account AUTO_INCREMENT=1000;
 
-CREATE TABLE locations (
+CREATE TABLE location (
 	location_id VARCHAR(8) NOT NULL,
 	location_name VARCHAR(30),
 	total_spaces INT,
@@ -19,22 +19,22 @@ CREATE TABLE locations (
     PRIMARY KEY (location_id));
 
 
-CREATE TABLE users (
-	user_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE driver (
+	driver_id INT NOT NULL AUTO_INCREMENT,
 	account_id INT NOT NULL,
 	college_id VARCHAR(10),
 	full_name VARCHAR(30),
-    PRIMARY KEY (user_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id));
-    ALTER TABLE users AUTO_INCREMENT=2000;
+    PRIMARY KEY (driver_id),
+    FOREIGN KEY (account_id) REFERENCES account(account_id));
+    ALTER TABLE driver AUTO_INCREMENT=2000;
 
-CREATE TABLE cars (
+CREATE TABLE car (
 	registration VARCHAR(15) NOT NULL,
 	account_id INT NOT NULL,
     PRIMARY KEY (registration),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id));
+    FOREIGN KEY (account_id) REFERENCES account(account_id));
 
-CREATE TABLE payments (
+CREATE TABLE payment (
     payment_id INT AUTO_INCREMENT NOT NULL,
 	account_id INT NOT NULL,
 	name_on_card VARCHAR(30),
@@ -42,8 +42,8 @@ CREATE TABLE payments (
 	card_expiry VARCHAR(5),
 	ccv INT,
     PRIMARY KEY (payment_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id));
-    ALTER TABLE payments AUTO_INCREMENT=3000;
+    FOREIGN KEY (account_id) REFERENCES account(account_id));
+    ALTER TABLE payment AUTO_INCREMENT=3000;
 
 CREATE TABLE transaction_history (
 	transaction_id INT NOT NULL AUTO_INCREMENT,
@@ -52,8 +52,8 @@ CREATE TABLE transaction_history (
 	location_id VARCHAR(8) NOT NULL,
 	cost DOUBLE(4,2),
     PRIMARY KEY (transaction_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
-    FOREIGN KEY (location_id) REFERENCES locations(location_id));
+    FOREIGN KEY (account_id) REFERENCES account(account_id),
+    FOREIGN KEY (location_id) REFERENCES location(location_id));
     ALTER TABLE transaction_history AUTO_INCREMENT=4000;
 
 CREATE TABLE car_entry (
@@ -63,7 +63,7 @@ CREATE TABLE car_entry (
 	image VARCHAR(25),
 	date_time TIMESTAMP,
     PRIMARY KEY (entry_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id));
+    FOREIGN KEY (account_id) REFERENCES account(account_id));
     ALTER TABLE car_entry AUTO_INCREMENT=5000;
 
 CREATE TABLE car_exit (
@@ -73,37 +73,37 @@ CREATE TABLE car_exit (
 	image VARCHAR(25),
 	date_time TIMESTAMP,
     PRIMARY KEY (exit_id),
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
-    FOREIGN KEY (registration) REFERENCES cars(registration));
+    FOREIGN KEY (account_id) REFERENCES account(account_id),
+    FOREIGN KEY (registration) REFERENCES car(registration));
     ALTER TABLE car_exit AUTO_INCREMENT=6000;
 
 
-INSERT INTO accounts VALUES(1000, 0, "d00230552@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
-INSERT INTO accounts (account_balance, email_address, password) VALUES(5, "d00229452@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
-INSERT INTO accounts (account_id, account_balance, email_address, password) VALUES(1003, 3.50, "d00197352@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
-INSERT INTO accounts (account_balance) VALUES(15.00);
-INSERT INTO accounts (account_balance) VALUES(7.50);
-INSERT INTO accounts VALUES(1002, 3.50, "d00230552@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
+INSERT INTO account VALUES(1000, 0, "d00230552@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
+INSERT INTO account (account_balance, email_address, password) VALUES(5, "d00229452@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
+INSERT INTO account (account_id, account_balance, email_address, password) VALUES(1003, 3.50, "d00197352@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
+INSERT INTO account (account_balance) VALUES(15.00);
+INSERT INTO account (account_balance) VALUES(7.50);
+INSERT INTO account VALUES(1002, 3.50, "d00230552@student.dkit.ie", "sha256$t9IJqsQidBrL50b6$79dbd976e97e250a6847e6cd71fb95fbdf6a5f7833db6cd5ce4e93137c8a65e5");
 
 
-INSERT INTO locations VALUES
+INSERT INTO location VALUES
 ("DKPJF256", "DkIT PJCarrolls Front", 400, 30),
 ("DKPJB652", "DkIT PJCarrolls Rear", 60, 5);
 
-INSERT INTO users VALUES
+INSERT INTO driver VALUES
 (2000, 1000, "D00230552", "Conor McGuire"),
 (2001, 1005, "D00229452", "Kamil Jozefowicz"),
 (2002, 1004, "D00197352", "Brian McKenna"),
 (2003, 1003, "D00230552", "Jacqueline O'Connor");
 
-INSERT INTO cars VALUES
+INSERT INTO car VALUES
 ("161LH12345", 1000),
 ("142LH54321", 1004),
 ("212LH678", 1003),
 ("11MH345", 1005),
 ("10D9393", 1002);
 
-INSERT INTO payments VALUES
+INSERT INTO payment VALUES
 (3001, 1000, "Conor McGuire", "1234567812345678", "12/24", "123"),
 (3002, 1005, "Kamil Jozefowicz", "8765432187654321", "12/24", "321"),
 (3003, 1002, "Brian McKenna", "1234567887654321", "12/24", "312");
